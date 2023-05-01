25f7bedb866ff705157449f0bc1f88ac874cd6b5
Zhan <galymzhan.aitkaliyev@gmail.com>
2023-05-01 23:45:51 +0600

    init commit

--- MyPhoto/PhotosCollectionViewController.swift
+++ MyPhoto/PhotosCollectionViewController.swift
@@ -25,12 +25,6 @@ class PhotosCollectionViewController: UICollectionViewController {
 
     /*
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
     */
 
     // MARK: UICollectionViewDataSource
@@ -42,15 +36,11 @@ class PhotosCollectionViewController: UICollectionViewController {
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
         return 0
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
-        // Configure the cell
-    
         return cell
     }
 
