4c508dd58c742da21772ac74c686008f11713f03
Zhan <galymzhan.aitkaliyev@gmail.com>
2023-05-06 18:43:13 +0600

    commented viewDidLoad changed UIEdgeInsets parameters

--- MyPhoto/PhotosCollectionViewController.swift
+++ MyPhoto/PhotosCollectionViewController.swift
@@ -11,6 +11,17 @@ class PhotosCollectionViewController: UICollectionViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+//        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+//
+//        layout.itemSize = CGSize(width: 70, height: 70)
+//        layout.sectionInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+//        layout.minimumLineSpacing = 1
+//        layout.minimumInteritemSpacing = 1
+//        layout.scrollDirection = .vertical
+//
+//        collectionView.showsVerticalScrollIndicator = true
+//
     }
     
     // MARK: UICollectionViewDataSource
@@ -28,7 +39,6 @@ class PhotosCollectionViewController: UICollectionViewController {
         cell.backgroundColor = .black
         return cell
     }
-    
 }
 
 extension PhotosCollectionViewController: UICollectionViewDelegateFlowLayout {
@@ -39,15 +49,15 @@ extension PhotosCollectionViewController: UICollectionViewDelegateFlowLayout {
         let widthPerItem = availableWidth / itemsPerRow
         return CGSize(width: widthPerItem, height: widthPerItem)
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 2, left: 2, bottom: 2, right: 2)
+        return UIEdgeInsets(top: 5, left: 2, bottom: 5, right: 2)
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 2
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return 2
     }
