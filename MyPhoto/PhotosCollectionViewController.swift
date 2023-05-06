2c2a9eb4c1f55ce9fb2592f3d5ca2b355c397fd0
Zhan <galymzhan.aitkaliyev@gmail.com>
2023-05-06 19:35:04 +0600

    added photo to assets set IBOutlet dogImageView set array photos

--- MyPhoto/PhotosCollectionViewController.swift
+++ MyPhoto/PhotosCollectionViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class PhotosCollectionViewController: UICollectionViewController {
     
+    let myPhotos = [#imageLiteral(resourceName: "dog15"), #imageLiteral(resourceName: "dog7"), #imageLiteral(resourceName: "dog5"), #imageLiteral(resourceName: "dog10"), #imageLiteral(resourceName: "dog6"), #imageLiteral(resourceName: "dog11"), #imageLiteral(resourceName: "dog9"), #imageLiteral(resourceName: "dog1"), #imageLiteral(resourceName: "dog8"), #imageLiteral(resourceName: "dog12"), #imageLiteral(resourceName: "dog3"), #imageLiteral(resourceName: "dog2"), #imageLiteral(resourceName: "dog14"), #imageLiteral(resourceName: "dog4"), #imageLiteral(resourceName: "dog13")]
+    let photos = ["dog1", "dog2", "dog3", "dog4", "dog5", "dog6", "dog7", "dog8", "dog9", "dog10", "dog11", "dog12", "dog13", "dog14", "dog15"]
     let itemsPerRow: CGFloat = 2
     let sectionInserts = UIEdgeInsets(top: 10, left: 2, bottom: 10, right: 2)
     
@@ -30,16 +32,21 @@ class PhotosCollectionViewController: UICollectionViewController {
     // MARK: UICollectionViewDataSource
     
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 2
+        return 1
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 30
+        return photos.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as! PhotoCell
-        cell.backgroundColor = .black
+        
+        let imageName = photos[indexPath.item]
+        let image = UIImage(named: imageName)
+        
+        cell.dogImageView.image = image
+        
         return cell
     }
 }
